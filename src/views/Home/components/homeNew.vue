e0125082f71edd3d96cb31dec3a5ac02fb8ab2c2
jie <1174697358@qq.com>
2026-05-09 14:40:27 +0800

    修改了整体排版

--- src/views/Home/components/homeNew.vue
+++ src/views/Home/components/homeNew.vue
@@ -1,5 +1,17 @@
 <template>
-  <homePanel :goodsList="freshGoodsList" title="新鲜好物" subTitle="新鲜出炉 品质靠谱"/>
+  <homePanel title="新鲜好物" subTitle="新鲜出炉 品质靠谱">
+    <ul>
+        <li class="item" v-for="item in freshGoodsList" :key="item.id">
+          <a href="/">
+            <img :src="item.picture" alt="" :title="item.name">
+            <div class="info">
+              <p class="title">{{item.name}}</p>
+              <p class="price"><span>¥</span>{{item.price}}</p>
+            </div>
+          </a>
+        </li>
+      </ul>
+  </homePanel>
  </template>
 
 <script setup>
@@ -15,5 +27,40 @@ onMounted(() => {
 })
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+ ul{
+      width: 100%;
+      height: 100%;
+      display: flex;
+      justify-content: space-between;
+      .item{
+        width: 303px;
+        height: 100%;
+        background: #EEF9F4;
+        img{
+          width: 303px;
+          height: 303px;
+        }
+        .info{
+          text-align: center;
+          .title{
+            font-size: 20px;
+            color: #333;
+            margin: 10px;
+            white-space: nowrap; /* 强制不换行 */
+  overflow: hidden;    /* 溢出部分隐藏 */
+  text-overflow: ellipsis; /* 溢出显示省略号... */
+  width: 280px;
+          }
+          .price{
+            font-size: 22px;
+            color: $priceColor;
+            span{
+              font-size: 18px;
+              margin-right: 5px;
+            }
+          }
+        }
+      }
+    }
 </style>
--- src/views/Home/components/homePanel.vue
+++ src/views/Home/components/homePanel.vue
@@ -4,17 +4,7 @@
       <h3>{{title}}<span>{{subTitle}}</span></h3>
     </div>
     <div class="body">
-      <ul>
-        <li class="item" v-for="item in goodsList" :key="item.id">
-          <a href="/">
-            <img :src="item.picture" alt="" :title="item.name">
-            <div class="info">
-              <p class="title">{{item.name}}</p>
-              <p class="price"><span>¥</span>{{item.price}}</p>
-            </div>
-          </a>
-        </li>
-      </ul>
+      <slot />
     </div>
   </div>
 </template>
@@ -56,41 +46,6 @@ defineProps({
   .body{
     width: 100%;
     height: 404px;
-    ul{
-      width: 100%;
-      height: 100%;
-      display: flex;
-      justify-content: space-between;
-      .item{
-        width: 303px;
-        height: 100%;
-        background: #EEF9F4;
-        img{
-          width: 303px;
-          height: 303px;
-        }
-        .info{
-          text-align: center;
-          .title{
-            font-size: 20px;
-            color: #333;
-            margin: 10px;
-            white-space: nowrap; /* 强制不换行 */
-  overflow: hidden;    /* 溢出部分隐藏 */
-  text-overflow: ellipsis; /* 溢出显示省略号... */
-  width: 280px;
-          }
-          .price{
-            font-size: 22px;
-            color: $priceColor;
-            span{
-              font-size: 18px;
-              margin-right: 5px;
-            }
-          }
-        }
-      }
-    }
   }
 }
 </style>
