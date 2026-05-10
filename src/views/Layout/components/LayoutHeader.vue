b95ad83f5c0de1564d22f0f3733671abc9431abc
jie <1174697358@qq.com>
2026-05-10 21:42:22 +0800

    一级路由配置

--- src/views/Layout/components/LayoutHeader.vue
+++ src/views/Layout/components/LayoutHeader.vue
@@ -8,7 +8,9 @@
         <li class="home">
           <RouterLink to="/">首页</RouterLink>
         </li>
-        <li v-for="item in categoryStore.categoryList" :key="item.id"> <RouterLink to="/">{{item.name}}</RouterLink> </li>
+        <li v-for="item in categoryStore.categoryList" :key="item.id">
+           <RouterLink :to="'/category/' + item.id">{{item.name}}</RouterLink> 
+        </li>
       </ul>
       <div class="search">
         <!-- <el-icon class="icon-search"><Search /></el-icon> -->
