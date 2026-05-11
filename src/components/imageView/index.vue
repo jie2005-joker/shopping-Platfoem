67f9c0c95c6d24d61b68d06f5ae5ed9d1b1c9548
jie <1174697358@qq.com>
2026-05-11 21:52:36 +0800

    图片蒙层效果实现

--- src/components/imageView/index.vue
+++ src/components/imageView/index.vue
@@ -1,6 +1,6 @@
 <script setup>
-import { ref } from 'vue'
-
+import { ref, watch } from 'vue'
+import { useMouseInElement } from '@vueuse/core'
 // 图片列表
 const imageList = [
   "https://yanxuan-item.nosdn.127.net/d917c92e663c5ed0bb577c7ded73e4ec.png",
@@ -16,6 +16,33 @@ const handleEnter = (index) => {
   activeIndex.value = index
   // console.log(activeIndex.value)
 }
+
+const target = ref(null)
+
+const { elementX, elementY, isOutside } = useMouseInElement(target)
+
+// 监听鼠标移动事件
+const left = ref(0)
+const top = ref(0)
+
+watch(([elementX, elementY]), ([elementX, elementY]) => {
+  // console.log(elementX, elementY)
+  if(isOutside.value) return
+  else{
+      if(elementX > 100 && elementX < 300){
+        left.value = elementX - 100
+      }
+      if(elementY > 100 && elementY < 300){
+        top.value = elementY - 100
+      }
+    
+      if(elementX < 100)  left.value = 0
+      if(elementX > 300)  left.value = 200
+    
+      if(elementY < 100)  top.value = 0
+      if(elementY > 300)  top.value = 200
+      }
+})
 </script>
 
 
@@ -25,7 +52,7 @@ const handleEnter = (index) => {
     <div class="middle" ref="target">
       <img :src="imageList[activeIndex]" alt="" />
       <!-- 蒙层小滑块 -->
-      <div class="layer" :style="{ left: `0px`, top: `0px` }" ></div>
+      <div class="layer" :style="{ left: `${left}px`, top: `${top}px` }" ></div>
     </div>
     <!-- 小图列表 -->
     <ul class="small">
