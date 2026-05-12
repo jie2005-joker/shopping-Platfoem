51a4cf2fce2e8dd2a03c2fb3cf97f6407c75e99f
jie <1174697358@qq.com>
2026-05-12 09:02:32 +0800

    pinia实现数据持久化管理

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import 'element-plus/theme-chalk/src/index.scss'
  import { lazyPlugin } from './directives'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import App from './App.vue'
 import router from './router'
 import { componentsPlugin } from './components'
@@ -16,8 +16,9 @@ const app = createApp(App)
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component)
 }
-
-app.use(createPinia())
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+app.use(pinia)
 app.use(router)
 app.use(lazyPlugin)
 app.use(componentsPlugin)
