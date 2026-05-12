f9d6597b24d770225554a6be086e1c1ace42ff34
jie <1174697358@qq.com>
2026-05-12 13:33:49 +0800

    购物车

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -5,7 +5,7 @@ import { ref } from 'vue'
 export const useCartStore = defineStore('cart', () => {
   const cartList = ref([])
   const addCart = (goods) => {
-   console.log("添加购物车",goods)
+  //  console.log("添加购物车",goods)
   // 检查购物车中是否有相同商品
   
     // 有相同商品，更新数量
@@ -16,7 +16,7 @@ export const useCartStore = defineStore('cart', () => {
       // 无相同商品，添加到购物车
       cartList.value.push(goods)
     }
-    console.log(cartList.value)
+    // console.log(cartList.value)
     // 保存到本地存储
     // localStorage.setItem('cartList', JSON.stringify(cartList.value))
   }
